f71d394a5b415a5888ae8ddec9f426c16be2c4c2
evil_morfar <evil_morfar@hotmail.com>
2020-04-23 12:29:22 +0200

    Changed build script to emulate the result of the github build

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -1,18 +1,33 @@
 #!/bin/sh
+# Builds the addon like the release builder an deploys to WoW addon folder.
 echo "Executing a $0"
+
+# Check .env
+if [ -f ".env" ]; then
+ . "./.env"
+elif [[ -f ".scripts/.env" ]]; then
+   . ".scripts/.env"
+else
+   echo "<WARNING> Couldn't find \".env\" file. This should contain a WOW_LOCATION variable with the game path.">&2
+fi
+
+# Fetch release script
 release_script=$(curl -s "https://raw.githubusercontent.com/BigWigsMods/packager/master/release.sh")
 
+# Package addon(s)
+echo "$release_script" | bash -s -- -r "$(pwd)/.tmp/Retail" -dolz
+echo "$release_script" | bash -s -- -r "$(pwd)/.tmp/Classic" -dolz -g 1.13.4
 
-# # -d: Skip Upload
-# # -z: Skip zip
-echo "$release_script" | bash -s -- -r "$(pwd)/.tmp/Retail" -do
-echo "$release_script" | bash -s -- -r "$(pwd)/.tmp/Classic" -do -g 1.13.4
+# Copy to WoW addons
+ADDON_LOC="$(pwd)"
+ADDON="$(basename $ADDON_LOC)"
+DEST="$WOW_LOCATION/_retail_/Interface/AddOns/$ADDON"
+robocopy "./.tmp/Retail/$ADDON" "$DEST" //s //purge //XD .* __*  //XF ?.* __* //NFL //NDL //NJH
 
-# # Move the zip(s)
-mv ./.tmp/Retail/*.zip "./.release/"
-mv ./.tmp/Classic/*.zip "./.release/"
-#
+DEST="$WOW_LOCATION/_classic_/Interface/AddOns/$ADDON"
+robocopy "./.tmp/Classic/$ADDON" "$DEST" //s //purge //XD .* __*  //XF ?.* __* //NFL //NDL //NJH
+
 # # And delete .tmp
 rm -r "./.tmp"
 
-# read
+read
